b8ab3acc82ed9187ab44e13b6d28d95f832c617b
Miguel Angel <68487934+Mundzuk-Uldin@users.noreply.github.com>
2026-01-25 16:48:47 -0500

    Portal code added

---
+++ Assets/Portals/Example/Scripts/Killbox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killbox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        other.transform.position = Vector3.zero;
+        other.transform.rotation = Quaternion.identity;
+        other.transform.localScale = Vector3.one;
+        var rb = other.GetComponent<Rigidbody>();
+        if (rb) {
+            rb.linearVelocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            rb.mass = 1;
+        }
+    }
+}
